f7071c06520bab7d0a00a43f32f81cd2f7d6243f
CloudRex <cloudrex@outlook.com>
2019-07-11 09:31:22 -0400

    Create Makefile

---
+++ Ion.IR/src/Stream.h
@@ -0,0 +1,28 @@
+#pragma once
+
+using namespace std;
+
+template <typename T>
+class Stream
+{
+protected:
+	int position;
+
+	std::array<T> items;
+
+	int size;
+
+	int resolvePosition(int position) {
+		if ()
+	}
+
+public:
+	Stream(T items[]) {
+		this->items = items;
+		this->size = this->items.size();
+	}
+
+	T next() {
+		return this->items[++this->position];
+	}
+};
---
+++ Makefile
@@ -0,0 +1,11 @@
+BASE_DIR := ./Ion.IR
+SRC_DIR := $(BASE_DIR)/src
+OBJ_DIR := $(BASE_DIR)/obj
+SRC_FILES := $(wildcard $(SRC_DIR)/*.cpp)
+OBJ_FILES := $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SRC_FILES))
+
+main: $(OBJ_FILES)
+	clang -o $@ $^
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
+	clang -c -o $@ $<
