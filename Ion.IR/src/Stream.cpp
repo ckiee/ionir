9497cbf8f8b498a6ae63935ea0f3694d2fde7dc8
CloudRex <cloudrex@outlook.com>
2019-07-11 09:31:31 -0400

    Use proper file extensions

--- Ion.IR/src/Stream.cpp
+++ Ion.IR/src/Stream.cpp
@@ -1,1 +1,28 @@
 #include "Stream.h"
+
+using namespace std;
+
+template <typename T>
+class Stream
+{
+protected:
+	int position;
+
+	std::array<T> items;
+
+	int size;
+
+	int resolvePosition(int position) {
+		if ()
+	}
+
+public:
+	Stream(T items[]) {
+		this->items = items;
+		this->size = this->items.size();
+	}
+
+	T next() {
+		return this->items[++this->position];
+	}
+};
--- Ion.IR/src/Token.h
+++ Ion.IR/src/Token.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include "TokenType.cpp"
+#include "TokenType.h"
 
 class Token
 {
