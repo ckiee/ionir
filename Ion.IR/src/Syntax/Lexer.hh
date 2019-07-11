0d2a712f195a400dca8c3d14de766dcae35c1f91
CloudRex <cloudrex@outlook.com>
2019-07-11 12:32:41 -0400

    Create Lexer class

---
+++ Ion.IR/src/Syntax/Lexer.hh
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include "Token.hh"
+
+using namespace std;
+
+class Lexer {
+private:
+    std::string input;
+
+protected:
+    std::string getInput() {
+        return this->input;
+    }
+
+public:
+    Lexer(std::string input) {
+        this->input = input;
+    }
+
+    std::vector<Token> tokenize() {
+        // TODO
+        std::vector<Token> tokens = {};
+
+        return tokens;
+    }
+};
