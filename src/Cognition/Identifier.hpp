2c7c85e5146100497bbd3dfe5b4c6bf4a3e30b40
CloudRex <cloudrex@outlook.com>
2019-08-01 14:31:05 -0400

    Do not make pair for map

--- src/Cognition/Identifier.hpp
+++ src/Cognition/Identifier.hpp
@@ -14,10 +14,10 @@ public:
     Identifier()
     {
         // Register primitive types.
-        this->primitiveTypes->insert(std::make_pair("void", PrimitiveType::Void));
-        this->primitiveTypes->insert(std::make_pair("int", PrimitiveType::Integer32));
-        this->primitiveTypes->insert(std::make_pair("string", PrimitiveType::String));
-        this->primitiveTypes->insert(std::make_pair("char", PrimitiveType::Character));
+        this->primitiveTypes->insert("void", PrimitiveType::Void);
+        this->primitiveTypes->insert("int", PrimitiveType::Integer32);
+        this->primitiveTypes->insert("string", PrimitiveType::String);
+        this->primitiveTypes->insert("char", PrimitiveType::Character);
     }
 
     bool isPrimitive(std::string value)
