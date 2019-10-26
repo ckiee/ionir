84f84a744cfee86a16f6eab79264ea695bb26eed
CloudRex <cloudrex@outlook.com>
2019-10-26 19:32:20 -0400

    Organize identifiers

---
+++ src/syntax/token_identifier.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "syntax/token_type.h"
+
+namespace ionir
+{
+class TokenIdentifier
+{
+public:
+    bool isSymbol(TokenType type)
+    {
+        // TODO
+
+        return false;
+    }
+};
+} // namespace ionir
---
+++ src/type_system/type_identifier.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace ionir
+{
+class TypeIdentifier
+{
+public:
+    bool isPrimitive(std::string type)
+    {
+        // TODO
+
+        return false;
+    }
+};
+} // namespace ionir
