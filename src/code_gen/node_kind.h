24e655aa39e05d1d5a298ab9332b9e3fb0671ebd
CloudRex <cloudrex@outlook.com>
2019-11-03 18:45:31 -0500

    Describe NodeKind enum

--- src/code_gen/node_kind.h
+++ src/code_gen/node_kind.h
@@ -6,14 +6,27 @@ enum class NodeKind
 {
 	Type,
 
+	/**
+	 * A block, possibly containing statements.
+	 */
 	Block,
 
+	/**
+	 * Function prototype used to declare externs
+	 * or actual functions.
+	 */
 	Prototype,
 
 	Function,
 
+	/**
+	 * An external function definition.
+	 */
 	Extern,
 
+	/**
+	 * An unsigned integer literal.
+	 */
 	LiteralInteger
 };
 }
