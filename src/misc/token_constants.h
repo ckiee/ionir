882a646705013cf8feb36bfa97645b5833f0bc14
CloudRex <cloudrex@outlook.com>
2019-11-04 18:45:41 +0000

    Mark symbols and keywords

--- src/misc/token_constants.h
+++ src/misc/token_constants.h
@@ -11,42 +11,61 @@ namespace ionir
 {
 class TokenConstants
 {
-private:
+protected:
 	std::map<std::string, TokenType> simple;
 
 	std::vector<std::pair<std::regex, TokenType>> complex;
+
+    std::vector<TokenType> symbols;
+
+    std::vector<TokenType> keywords;
 
 	void pushComplex(std::regex regex, TokenType tokenType)
 	{
 		this->complex.push_back(std::make_pair(regex, tokenType));
 	}
 
+    void pushSimple(std::string value, TokenType type)
+    {
+        this->simple[value] = type;
+    }
+
+    void pushSymbol(std::string value, TokenType type)
+    {
+        this->pushSimple(value, type);
+        this->symbols.push_back(type);
+    }
+
+    void pushKeyword(std::string value, TokenType type)
+    {
+        this->pushSimple(value, type);
+        this->keywords.push_back(type);
+    }
+
 public:
 	TokenConstants()
 	{
 		// Register symbols.
-		this->simple["@"] = TokenType::SymbolAt;
-		this->simple[":"] = TokenType::SymbolColon;
-		this->simple["$"] = TokenType::SymbolDollar;
-		this->simple["#"] = TokenType::SymbolHash;
-		this->simple["("] = TokenType::SymbolParenthesesL;
-		this->simple[")"] = TokenType::SymbolParenthesesR;
-		this->simple["["] = TokenType::SymbolBracketL;
-		this->simple["]"] = TokenType::SymbolBracketR;
-		this->simple[","] = TokenType::SymbolComma;
-		this->simple["~"] = TokenType::SymbolTilde;
-		this->simple["="] = TokenType::SymbolEqual;
-		this->simple["%"] = TokenType::SymbolPercent;
-		this->simple[";"] = TokenType::SymbolSemiColon;
-
-		// Register instructions.
-		this->simple["create"] = TokenType::InstCreate;
-		this->simple["call"] = TokenType::InstCall;
-		this->simple["set"] = TokenType::InstSet;
-		this->simple["end"] = TokenType::InstEnd;
+        this->pushSymbol("@", TokenType::SymbolAt);
+        this->pushSymbol(":", TokenType::SymbolColon);
+        this->pushSymbol("$", TokenType::SymbolDollar);
+        this->pushSymbol("#", TokenType::SymbolHash);
+        this->pushSymbol("(", TokenType::SymbolParenthesesL);
+        this->pushSymbol(")", TokenType::SymbolParenthesesR);
+        this->pushSymbol("[", TokenType::SymbolBracketL);
+        this->pushSymbol("]", TokenType::SymbolBracketR);
+        this->pushSymbol(",", TokenType::SymbolComma);
+        this->pushSymbol("~", TokenType::SymbolTilde);
+        this->pushSymbol("=", TokenType::SymbolEqual);
+        this->pushSymbol("%", TokenType::SymbolPercent);
+        this->pushSymbol(";", TokenType::SymbolSemiColon);
 
 		// Register keywords.
-		this->simple["func"] = TokenType::KeywordFunction;
+        this->pushKeyword("create", TokenType::InstCreate);
+        this->pushKeyword("call", TokenType::InstCall);
+        this->pushKeyword("set", TokenType::InstSet);
+        this->pushKeyword("end", TokenType::InstEnd);
+        this->pushKeyword("func", TokenType::KeywordFunction);
 
 		// Initialize complex map.
 		this->pushComplex(Regex::identifier, TokenType::Identifier);
