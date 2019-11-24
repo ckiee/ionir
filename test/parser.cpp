fbf8f09c0ff1fcbbe7441c276d782fd2116a49ef
CloudRex <cloudrex@outlook.com>
2019-11-23 23:01:05 -0500

    Add parseInt() and parseChar() tests

--- test/parser.cpp
+++ test/parser.cpp
@@ -8,7 +8,7 @@ using namespace ::testing;
 
 namespace test = ionir::testing;
 
-TEST(ParserTest, TTTd)
+TEST(ParserTest, ParseInt)
 {
     std::vector<ionir::Token> tokens = {
         ionir::Token(ionir::TokenType::LiteralInt, "5", 0),
@@ -16,4 +16,22 @@ TEST(ParserTest, TTTd)
 
     ionir::TokenStream stream = ionir::TokenStream(tokens);
     ionir::Parser parser = ionir::Parser(stream);
+    ionir::LiteralInt *integer = parser.parseInt();
+
+    EXPECT_EQ(integer->getValue(), 5);
+}
+
+TEST(ParserTest, ParseChar)
+{
+    std::vector<ionir::Token> tokens = {
+        ionir::Token(ionir::TokenType::SymbolSingleQuote, "'", 0),
+        ionir::Token(ionir::TokenType::LiteralCharacter, "a", 1),
+        ionir::Token(ionir::TokenType::SymbolSingleQuote, "'", 2),
+    };
+
+    ionir::TokenStream stream = ionir::TokenStream(tokens);
+    ionir::Parser parser = ionir::Parser(stream);
+    ionir::LiteralChar *character = parser.parseChar();
+
+    EXPECT_EQ(character->getValue(), 'a');
 }
