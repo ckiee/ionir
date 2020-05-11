678dff53c2eb0db6a05697b6bac5344bc31933d1
CloudRex <cloudrex@outlook.com>
2020-05-11 05:28:26 -0400

    Fix implementation of the EntryPointCheckPass::visitFunction() method

--- include/ionir/passes/semantic/entry_point_check_pass.h
+++ include/ionir/passes/semantic/entry_point_check_pass.h
@@ -9,23 +9,6 @@ namespace ionir {
         OptPtr<Function> entryFunction;
 
     public:
-        void visitFunction(Ptr<Function> node) override {
-            if (node->getPrototype()->getId() == ConstName::main) {
-                /**
-                 * Make sure there was not an entry point function
-                 * previously encountered.
-                 */
-                if (Util::hasValue(this->entryFunction)) {
-                    throw std::runtime_error("Entry point function had already previously been declared");
-                }
-
-                this->entryFunction = node;
-            }
-
-            /**
-             * TODO: Missing code for when the entry point function
-             * is not found.
-             */
-        }
+        void visitFunction(Ptr<Function> node) override;
     };
 }
--- src/passes/semantic/entry_point_check_pass.cpp
+++ src/passes/semantic/entry_point_check_pass.cpp
@@ -3,9 +3,24 @@
 #include <ionir/passes/semantic/entry_point_check_pass.h>
 
 namespace ionir {
-    void EntryPointCheckPass::visitInst(Ptr<Inst> node) {
-        Pass::visitInst(node);
+    void EntryPointCheckPass::visitFunction(Ptr<Function> node) {
+        Pass::visitFunction(node);
 
-        // TODO
+        if (node->getPrototype()->getId() == ConstName::main) {
+            /**
+             * Make sure there was not an entry point function
+             * previously encountered.
+             */
+            if (Util::hasValue(this->entryFunction)) {
+                throw std::runtime_error("Entry point function had already previously been declared");
+            }
+
+            this->entryFunction = node;
+        }
+
+        /**
+         * TODO: Missing code for when the entry point function
+         * is not found.
+         */
     }
 }
